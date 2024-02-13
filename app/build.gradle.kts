cd30a33841b6d6be80e67306686018a050eff1f6
Mehrdad S.F. <meddisommer@gmail.com>
2024-02-13 09:33:26 +0330

    Add needed resource files - second commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.instagramexploresample"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.instagramexploresample"
         minSdk = 28
@@ -37,11 +41,17 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Picasso library
+    implementation("com.squareup.picasso:picasso:2.8")
+
+    // GLide library
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
--- app/src/main/java/com/example/instagramexploresample/MainActivity.kt
+++ app/src/main/java/com/example/instagramexploresample/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.instagramexploresample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.instagramexploresample.databinding.ActivityMainBinding
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
+
 }
